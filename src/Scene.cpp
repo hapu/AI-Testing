e328e6cc9a40b387a3719448eadb77e55912a090
hapu <hapu.x2@gmail.com>
2016-04-16 23:48:48 -0700

    tiny fix in movement

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -38,74 +38,73 @@ char Scene::map[20*20] = {
     2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,
 };
 
-void Scene::init()
-{
-    glewInit();
-    glEnable(GL_DEPTH_TEST);
-    glClearColor(0.9f, 0.9f, 1.0f, 1.0f);
-    camera.init(24.0, 15.0, 24.0, -0.785398, 0.785398, 0.00);
-    cube.init();
-    tetra.init();
-	tetra.rotation.x = glm::radians(180.0f);
-    tetra.position.y++;
-	tetra.position.x = 19.0;
-	tetra.position.z = 19.0;
+void Scene::init() {
+  glewInit();
+  glEnable(GL_DEPTH_TEST);
+  glClearColor(0.9f, 0.9f, 1.0f, 1.0f);
+  camera.init(24.0, 15.0, 24.0, -0.785398, 0.785398, 0.00);
+  cube.init();
+  tetra.init();
+  tetra.rotation.x = glm::radians(180.0f);
+  tetra.position.y++;
+  tetra.position.x = 19.0;
+  tetra.position.z = 19.0;
 }
 
-void Scene::update(float dt)
-{
-	glm::vec4 mov;
-	glm::vec3 last_pos = tetra.position;
-    //updateCamera(dt);
-	float speed = 5.0;
-	if (InputManager::keyboard.up)
-		mov.z -= speed * dt;
-	if (InputManager::keyboard.down)
-		mov.z += speed * dt;
-	if (InputManager::keyboard.right)
-		mov.x += speed * dt;
-	if (InputManager::keyboard.left)
-		mov.x -= speed * dt;
+void Scene::update(float dt) {
+  glm::vec4 tmp_movement(0.0f, 0.0f, 0.0f, 0.0f);
+  glm::vec3 tmp_position = tetra.position;
+  // updateCamera(dt);
+  float speed = 5.0;
+  if (InputManager::keyboard.up) tmp_movement.z -= speed;
+  if (InputManager::keyboard.down) tmp_movement.z += speed;
+  if (InputManager::keyboard.right) tmp_movement.x += speed;
+  if (InputManager::keyboard.left) tmp_movement.x -= speed;
 
-	mov = glm::rotate(camera.rotation.y, glm::vec3(0.0f, 1.0f, 0.0f)) * mov;
+  // tmp_movement = glm::normalize(tmp_movement);
+  tmp_movement *= dt;
+  tmp_movement = glm::rotate(camera.rotation.y, glm::vec3(0.0f, 1.0f, 0.0f)) * tmp_movement;
 
-	tetra.position.z += mov.z;
-	tetra.position.x += mov.x;
+  tmp_position.z += tmp_movement.z;
+  tmp_position.x += tmp_movement.x;
 
-	int x = tetra.position.x + 0.5;
-	int y = tetra.position.z + 0.5;
-	if (map[y * 20 + x] != 1)
-		tetra.position = last_pos;
+  int x = tmp_position.x + 0.5;
+  int z_orig = tetra.position.z + 0.5;
+  if (map[z_orig * 20 + x] == 1) tetra.position.x = tmp_position.x;
 
+  int x_orig = tetra.position.x + 0.5;
+  int z = tmp_position.z + 0.5;
+  if (map[z * 20 + x_orig] == 1) tetra.position.z = tmp_position.z;
 }
 
-void Scene::render()
-{
-    // It clears the screen
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    cube.render(camera, map, 20);
-    tetra.render(camera);
+void Scene::render() {
+  // It clears the screen
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  cube.render(camera, map, 20);
+  tetra.render(camera);
 }
 
 void Scene::cleanUp() {}
 
-void Scene::updateCamera(float dt)
-{
-   float rot_speed = InputManager::keyboard.lctrl? 6.0f : 3.0f;
-   if (InputManager::keyboard.right) camera.rotation.y -= rot_speed * dt;
-   if (InputManager::keyboard.left) camera.rotation.y += rot_speed * dt;
-   if (InputManager::keyboard.up) camera.rotation.x += rot_speed * dt;
-   if (InputManager::keyboard.down) camera.rotation.x -= rot_speed * dt;
-   if (InputManager::keyboard.rshift) camera.scale.z += rot_speed * dt;
-   if (InputManager::keyboard.rctrl) camera.scale.z -= rot_speed * dt;
-   glm::vec4 tmp_movement(0.0f, 0.0f, 0.0f, 0.0f);
-   if (InputManager::keyboard.w) tmp_movement.z -= rot_speed * dt;
-   if (InputManager::keyboard.s) tmp_movement.z += rot_speed * dt;
-   if (InputManager::keyboard.a) tmp_movement.x -= rot_speed * dt;
-   if (InputManager::keyboard.d) tmp_movement.x += rot_speed * dt;
-   if (InputManager::keyboard.lshift) tmp_movement.y -= rot_speed * dt;
-   if (InputManager::keyboard.space) tmp_movement.y += rot_speed * dt;
-   tmp_movement = glm::rotate(camera.rotation.y, glm::vec3(0.0f, 1.0f, 0.0)) * tmp_movement;
-   camera.position += glm::vec3(tmp_movement.x, tmp_movement.y, tmp_movement.z);
-   //printf("camera.init(%.2f, %.2f, %.2f, %.2f, %.2f, %.2f)\r\n", camera.position.x, camera.position.y, camera.position.z, camera.rotation.x, camera.rotation.y, camera.rotation.z);
+void Scene::updateCamera(float dt) {
+  float rot_speed = InputManager::keyboard.lctrl ? 6.0f : 3.0f;
+  if (InputManager::keyboard.right) camera.rotation.y -= rot_speed * dt;
+  if (InputManager::keyboard.left) camera.rotation.y += rot_speed * dt;
+  if (InputManager::keyboard.up) camera.rotation.x += rot_speed * dt;
+  if (InputManager::keyboard.down) camera.rotation.x -= rot_speed * dt;
+  if (InputManager::keyboard.rshift) camera.scale.z += rot_speed * dt;
+  if (InputManager::keyboard.rctrl) camera.scale.z -= rot_speed * dt;
+  glm::vec4 tmp_movement(0.0f, 0.0f, 0.0f, 0.0f);
+  if (InputManager::keyboard.w) tmp_movement.z -= rot_speed * dt;
+  if (InputManager::keyboard.s) tmp_movement.z += rot_speed * dt;
+  if (InputManager::keyboard.a) tmp_movement.x -= rot_speed * dt;
+  if (InputManager::keyboard.d) tmp_movement.x += rot_speed * dt;
+  if (InputManager::keyboard.lshift) tmp_movement.y -= rot_speed * dt;
+  if (InputManager::keyboard.space) tmp_movement.y += rot_speed * dt;
+  tmp_movement =
+      glm::rotate(camera.rotation.y, glm::vec3(0.0f, 1.0f, 0.0)) * tmp_movement;
+  camera.position += glm::vec3(tmp_movement.x, tmp_movement.y, tmp_movement.z);
+  // printf("camera.init(%.2f, %.2f, %.2f, %.2f, %.2f, %.2f)\r\n",
+  // camera.position.x, camera.position.y, camera.position.z, camera.rotation.x,
+  // camera.rotation.y, camera.rotation.z);
 }
--- src/Scene.hpp
+++ src/Scene.hpp
@@ -6,19 +6,19 @@
 #include <MinecraftCube.hpp>
 #include <Tetrahedron.hpp>
 
-class Scene
-{
-    private:
-        static Camera camera;
-        static MinecraftCube cube;
-        static Tetrahedron tetra;
-    public:
-        static void init();
-        static void update(float dt = 0.0f);
-        static void render();
-        static void cleanUp();
-        static void updateCamera(float dt);
-        static char map[20*20];
+class Scene {
+ private:
+  static Camera camera;
+  static MinecraftCube cube;
+  static Tetrahedron tetra;
+
+ public:
+  static void init();
+  static void update(float dt = 0.0f);
+  static void render();
+  static void cleanUp();
+  static void updateCamera(float dt);
+  static char map[20 * 20];
 };
 
 #endif  //_SCENE_H_
